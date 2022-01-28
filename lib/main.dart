e8006994ad3134a4a59616d4d0ad9b4f76e467e1
DEV BATHANI <bathanid888@gmail.com>
2022-01-28 12:51:10 +0530

    Final code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:schaffen_assignment/Database/DatabaseManager.dart';
+import 'package:schaffen_assignment/UI/home_screen.dart';
 
 import 'UI/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DatabaseManager(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,15 +29,49 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
+    return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: () => const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Schaffen Softwares',
-        home: Splashscreen(),
+        home: Initializer(),
       ),
     );
+  }
+}
+
+class Initializer extends StatefulWidget {
+  const Initializer({Key? key}) : super(key: key);
+
+  @override
+  _InitializerState createState() => _InitializerState();
+}
+
+class _InitializerState extends State<Initializer> {
+  late FirebaseAuth _auth;
+  User? _user;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth = FirebaseAuth.instance;
+    _user = _auth.currentUser;
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : _user == null
+            ? const Splashscreen()
+            : const HomeScreen();
   }
 }
